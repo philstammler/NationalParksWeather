a12bf71f04af5c6ae00827d5db7eb365fd81b92a
Jacob Bulicek <toucanjake@yahoo.com>
2019-07-10 12:59:46 -0400

    Added interface for survey results

---
+++ 13-Capstone/Capstone.Web/DAL/ISurvey_ResultSqlDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Capstone.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.DAO
+{
+    public interface ISurvey_ResultSqlDao
+    {
+        List<Survey_Result> GetAllSurveys();
+        bool AddResult(Survey_Result result);
+        List<SurveyResultViewModel> GetTopRankedParks();
+    }
+}
