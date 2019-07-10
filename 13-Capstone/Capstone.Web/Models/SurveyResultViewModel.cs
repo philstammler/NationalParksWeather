752534cc673678d2e4269090866f362cd6bcc104
Jacob Bulicek <toucanjake@yahoo.com>
2019-07-10 12:52:08 -0400

    SurveyResultViewModel Class

---
+++ 13-Capstone/Capstone.Web/Models/SurveyResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class SurveyResultViewModel
+    {
+        public string ParkCode { get; set; }
+        public string ParkName { get; set; }
+        public int VoteCount { get; set; }
+    }
+}
