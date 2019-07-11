279ada90f6e85a408d0416a52df89504209d5421
Jacob Bulicek <toucanjake@yahoo.com>
2019-07-11 10:19:40 -0400

    Trying something new, might revert if problematic.

--- 13-Capstone/Capstone.Web/Models/Survey_Result.cs
+++ 13-Capstone/Capstone.Web/Models/Survey_Result.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -21,6 +22,7 @@ namespace Capstone.Web.Models
         [MinLength(2)]
         [Required]
         public string State { get; set; }
+        public IEnumerable<SelectListItem> States { get; set; }
 
         [Required]
         public string ActivityLevel { get; set; }
